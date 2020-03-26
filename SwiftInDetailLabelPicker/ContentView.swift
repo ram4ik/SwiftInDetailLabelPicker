1453172303146abe22db43fe1b3e23e95ebcfdfe
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-26 13:10:57 +0200

    init

--- SwiftInDetailLabelPicker/ContentView.swift
+++ SwiftInDetailLabelPicker/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Picker")
+                .font(.headline)
+            Picker(selection: .constant(1), label: Text("Picker")) {
+                Text("1").tag(1)
+                Text("1").tag(2)
+            }
+        }.padding()
     }
 }
 
